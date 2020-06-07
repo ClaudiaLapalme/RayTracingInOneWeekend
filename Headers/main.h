3719ccf77d003006481a4a812832b66a5e6674b6
Claudia <claudia.lapalme@gmail.com>
2020-06-07 16:23:15 -0400

    Print rainbow picture + progress updates

--- Headers/main.h
+++ Headers/main.h
@@ -5,9 +5,13 @@
 #ifndef RAYTRACINGINONEWEEKEND_MAIN_H
 #define RAYTRACINGINONEWEEKEND_MAIN_H
 
+#include <iostream>
+#include <fstream>
 
 class main {
-
+private:
+public:
+    static void printImage(int image_width, int image_height);
 };
 
 
--- Sources/main.cpp
+++ Sources/main.cpp
@@ -3,8 +3,33 @@
 //
 
 #include "../Headers/main.h"
-#include <iostream>
 
 int main() {
-    std::cout << "hello world" << std::endl;
+    main::printImage(256, 256);
+}
+
+void main::printImage(const int image_width, const int image_height) {
+
+    std::ofstream out("..\\Output\\helloWorld.ppm");
+    std::streambuf *coutbuf = std::cout.rdbuf(); //save old buf
+    std::cout.rdbuf(out.rdbuf()); //redirect std::cout to out.txt!
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = image_height - 1; j >= 0; j--) { // top to bottom
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        for (int i = 0; i < image_width; i++) { // left to right
+            auto r = static_cast<double>(i) / (image_width - 1);
+            auto g = static_cast<double>(j) / (image_height - 1);
+            auto b = 0.25;
+
+            int ir = static_cast<int> (255.999 * r);
+            int ig = static_cast<int> (255.999 * g);
+            int ib = static_cast<int> (255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << std::endl;
+        }
+    }
+    std::cout.rdbuf(coutbuf); //reset to standard output again
+    std::cerr << "\nThe image has been produced.\n";
 }
