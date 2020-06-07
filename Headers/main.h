3ac6ce90b2921abfba9ede70682eec7c2d26f600
Claudia <claudia.lapalme@gmail.com>
2020-06-07 14:32:50 -0400

    Project setup

---
+++ Headers/main.h
@@ -0,0 +1,14 @@
+//
+// Created by claul on 6/7/2020.
+//
+
+#ifndef RAYTRACINGINONEWEEKEND_MAIN_H
+#define RAYTRACINGINONEWEEKEND_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //RAYTRACINGINONEWEEKEND_MAIN_H
---
+++ Sources/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by claul on 6/7/2020.
+//
+
+#include "../Headers/main.h"
+#include <iostream>
+
+int main() {
+    std::cout << "hello world" << std::endl;
+}
